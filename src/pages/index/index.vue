4b701a131aace2f7d8a5b207387d4f2b61690a65
BlackCyanC <17688018176@163.com>
2019-01-27 10:57:43 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="index-container">
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/pages/mine/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="mine-container">
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
